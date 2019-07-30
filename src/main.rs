548fe99781117c19fcb001ca7f6122c91dafe1c6
yasushisakai <ysshski@gmail.com>
2019-07-30 15:17:30 -0400

    token based authbash-3.2$ git add .

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 use reqwest;
+use reqwest::Client;
+use reqwest::header::AUTHORIZATION;
 use serde_json::Value;
 use cs_cityio_backend::{connect, send_table};
 use regex::Regex;
 use std::{thread, time};
+use std::env;
+use dotenv::dotenv;
+
 
 const BASE_URL: &str = "https://cityio.media.mit.edu/api";
 
 fn main() {
+
+    dotenv().ok();
+
+    let cityio_module_key = env::var("CITYIO_MODULE_KEY").unwrap();
+    println!("{}", &cityio_module_key);
 
     let list_end_point = format!("{}/tables/list", BASE_URL);
 
@@ -20,7 +30,16 @@ fn main() {
             .expect("Could not parse table list json to data");
 
         for url in resp {
-            let table_data: Value = reqwest::get(&url)
+
+            println!("{}", &url);
+
+
+            let token = format!("Bearer {}", &cityio_module_key);
+
+            let client = Client::new();
+            let table_data: Value = client.get(&url)
+                .header(AUTHORIZATION, token)
+                .send()
                 .expect(&format!("Error getting table {}", &url))
                 .json()
                 .expect("Could not parse table to json");
@@ -31,12 +50,14 @@ fn main() {
             let re = Regex::new(r"(\w*).$").unwrap();
 
             let table_name = re.captures(&url).map(|c| {
-                format!("{:?}", c.get(0).map(|m| m.as_str()))
+                let tn = c.get(0).map(|m| m.as_str());
+                tn.unwrap()
             }).unwrap();
 
             let id = get_id(&table_data).unwrap();
 
             let con = connect();
+            println!("{}", &table_name);
             match send_table(&con, id, &table_name, &table_data) {
                 Ok(()) => (),
                 Err(e) => println!("{}", e),
